7c1deae5d11deddd31b7f43db811f45590c47297
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-11-02 22:24:17 -0300

    se agregó condicional al control de presupeusto

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import Header from './components/Header'
 
 const App = () => {
   const [presupuesto, setPresupuesto] = useState(0)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
   return (
     <>
       <Header
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
       />
       
     </>
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,17 +1,19 @@
 import React, {useState} from 'react'
 import Mensaje from './Mensaje'
 
-const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto,setIsValidPresupuesto}) => {
 
   const [mensaje, setMensaje] = useState("")
 
   const onSubmit = (e) => {
     e.preventDefault();
-    if (!Number(presupuesto) || Number(presupuesto) < 0) {
-      setMensaje("No es un presupuesto valido")      
-    }else{
-      setMensaje("Si es un presupuesto valido")
+    if (!presupuesto || presupuesto < 0) {
+      setMensaje("No es un presupuesto valido");
+      setIsValidPresupuesto(false);
+      return;      
     }
+    setMensaje("");
+    setIsValidPresupuesto(true);
   }
   return (
     <>
@@ -20,11 +22,11 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
           <div className="campo">
             <label htmlFor="">Definir Presupuesto</label>
             <input 
-              type="text" 
+              type="number" 
               className="nuevo-presupuesto" 
               placeholder='Añade tu Presupuesto'
               value={presupuesto}
-              onChange={ (e) => setPresupuesto(e.target.value)}
+              onChange={ (e) => setPresupuesto(Number(e.target.value))}
             />
             <input 
               type="submit" 
