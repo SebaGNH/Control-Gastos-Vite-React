a2633f345922a47dc7fd707c18e8f9cbbe60c891
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-11-21 20:37:34 -0300

    se agregaron campos para formulario en ventana modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -21,6 +21,38 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
       </div>
       <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
         <legend>Nuevo Gasto</legend>
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+          <input 
+            type="text"
+            id="nombre"
+            placeholder="Añade el Nombre del Gasto"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+          <input 
+            type="text"
+            id="cantidad"
+            placeholder="Añade la cantidad del gasto: ej: 300"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="categoria">Categoría</label>
+          <select
+            id="categoria"
+          >
+            <option value="">-- Seleccione una Categoría --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+        <input type="submit" value="Añadir Gasto" />
       </form>
     </div>
   )
